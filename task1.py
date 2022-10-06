1a09c84d6d45da82e7706fd00e073bb8abed019d
Nicky <92157053+Nick4uogl@users.noreply.github.com>
2022-10-06 10:38:19 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,6 +8,8 @@ args = parser.parse_args()
 
 match args.operator:
     case "/":
+        if args.y == 0:
+            raise ValueError('Not allowed to divide by zero')
         print(args.x / args.y)
     case "-":
         print(args.x - args.y)
